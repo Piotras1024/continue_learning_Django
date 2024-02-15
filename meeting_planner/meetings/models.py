023e42b5cfe34059ba90d8e39a4ddf0bf61a302f
piotr.w.dabrowski2@gmail.com <Piotr Dbrowski>
2024-02-15 08:44:01 +0100

    First commit

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import time
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    floor_number = models.IntegerField(default=0)
+    room_number = models.IntegerField(default=0)
+
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = forei
+    def __str__(self):
+        return f"{self.title} start at {self.start_time} it will take {self.duration} at {self.date}"
+
 
 # Create your models here.
